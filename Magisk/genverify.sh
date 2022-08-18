a891089698936cd0e0d2201d273e617800d61385
sekaiacg <sekaiacg@gmail.com>
2022-08-19 07:59:46 +0800

    Magisk: initial commit
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

---
+++ Magisk/genverify.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sha256sum Magisk.apk > verify.sha256sum
+sha256sum MagiskAlpha.apk >> verify.sha256sum
+sha256sum MagiskVer >> verify.sha256sum
+sha256sum MagiskAlphaVer >> verify.sha256sum
