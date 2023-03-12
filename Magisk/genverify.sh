da4cbe33c2d8c8909de2baa859ecb6960bb523b5
sekaiacg <sekaiacg@gmail.com>
2023-03-12 10:33:53 +0800

    MagiskDelta: Add ed8e3234-delta[25210]
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- Magisk/genverify.sh
+++ Magisk/genverify.sh
@@ -4,3 +4,5 @@ sha256sum MagiskVer > verify.sha256sum
 sha256sum Magisk.apk >> verify.sha256sum
 sha256sum MagiskAlphaVer >> verify.sha256sum
 sha256sum MagiskAlpha.apk >> verify.sha256sum
+sha256sum MagiskDeltaVer >> verify.sha256sum
+sha256sum MagiskDelta.apk >> verify.sha256sum
